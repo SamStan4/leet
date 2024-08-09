02bcef2fae63aa72386ea5185ecd80a5e2984f2f
SamStan4 <sam.charles.stanley@gmail.com>
2024-08-08 23:59:48 -0700

    done with spiral BS

--- medium/spiral_matrix_iii.hpp
+++ medium/spiral_matrix_iii.hpp
@@ -7,27 +7,28 @@ class Solution
 public:
     vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart)
     {
-        vector<vector<int>> visits = {{rStart, cStart}}, dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int row = rStart, col = cStart, to_add = rows * cols, turns = 1, line_distance = 0;
+        vector<vector<int>> locations = {{rStart, cStart}}, dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int r = rStart, c = cStart, target = rows * cols, turns = 1, line_add = 0;
 
-        while (visits.size() < to_add)
+        while (locations.size() < target)
         {
             for (int i = 0; i < 4; ++i, ++turns)
             {
-                line_distance = (int)ceil((double)turns / 2.0);
-                row += dirs[i][0];
-                col += dirs[i][1];
+                line_add = (int)ceil((double)turns / 2.0);
 
-                for (int j = 0; j < line_distance; ++j, row += dirs[i][0], col += dirs[i][1])
+                for (int j = 0; j < line_add; ++j)
                 {
-                    if ((row >= 0) && (col >= 0) && (row < rows) && (col < cols))
+                    r += dirs[i][0];
+                    c += dirs[i][1];
+
+                    if ((r >= 0) && (c >= 0) && (r < rows) && (c < cols) && (locations.size() < target))
                     {
-                        visits.push_back({row, col});
+                        locations.push_back({r, c});
                     }
                 }
             }
-        }
+        }  
 
-        return visits;
+        return locations;
     }
 };
