f2b12588ac9bc2489c0d6d1d901add2a2c18fa16
samStan4 <sam.charles.stanley@gmail.com>
2024-08-09 02:55:32 -0700

    8/9/24

--- medium/magic_squares_in_grid.hpp
+++ medium/magic_squares_in_grid.hpp
@@ -4,11 +4,92 @@
 
 class Solution
 {
+private:
+    int is_magic(vector<vector<int>>& g, int r, int c)
+    {
+        bool nums[9] = { false };
+        int temp = 0;
+
+        if ((g[r + 1][c + 1] != 5) || (g[r][c] & 1) || (g[r][c + 2] & 1) || (g[r + 2][c] & 1) || (g[r + 2][c + 2] & 1))
+        {
+            return 0;
+        }
+
+        for (int i = 0; i < 3; ++i)
+        {
+            for (int j = 0; j < 3; ++j)
+            {
+                temp = g[r + i][c + j];
+
+                if ((temp > 9) || (temp < 1))
+                {
+                    return 0;
+                }
+                else
+                {
+                    nums[temp - 1] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < 9; ++i)
+        {
+            nums[0] = nums[0] && nums[i];
+        }
+
+        if (!nums[0])
+        {
+            return 0;
+        }
+
+        if ((g[r][c] + g[r][c+1] + g[r][c+2]) != 15)
+        {
+            return 0;
+        }
+
+        if ((g[r+1][c] + g[r+1][c+1] + g[r+1][c+2]) != 15)
+        {
+            return 0;
+        }
+
+        if ((g[r+2][c] + g[r+2][c+1] + g[r+2][c+2]) != 15)
+        {
+            return 0;
+        }
+
+        if ((g[r][c] + g[r+1][c] + g[r+2][c]) != 15)
+        {
+            return 0;
+        }
+
+        if ((g[r][c+1] + g[r+1][c+1] + g[r+2][c+1]) != 15)
+        {
+            return 0;
+        }
+
+        if ((g[r][c+2] + g[r+1][c+2] + g[r+2][c+2]) != 15)
+        {
+            return 0;
+        }
+
+        if ((g[r][c] + g[r+2][c+2]) != 10)
+        {
+            return 0;
+        }
+
+        if ((g[r+2][c] + g[r][c+2]) != 10)
+        {
+            return 0;
+        }
+
+        return 1;
+    }
+
 public:
 
     int numMagicSquaresInside(vector<vector<int>>& grid)
     {
-        int n  = grid.size() - 2, m = grid[0].size() - 2, s1 = 0, s2 = 0, s3 = 0;
+        int n  = grid.size() - 2, m = grid[0].size() - 2, count = 0;
 
         for (int i = 0; i < n; ++i)
         {
@@ -16,10 +97,11 @@ public:
             {
                 if ((grid[i+1][j+1] == 5) && !(grid[i][j] & 1) && !(grid[i+2][j+2] & 1) && !(grid[i+2][j] & 1) && !(grid[i][j+2] & 1))
                 {
+                    count += this->is_magic(grid, i, j);
                 }
             }
         }
 
-        return 0;
+        return count;
     }
 };
