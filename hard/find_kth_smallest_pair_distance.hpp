198b75986e07c5a2ca2503d4ee0712edb3663ec4
samStan4 <sam.charles.stanley@gmail.com>
2024-08-14 01:09:50 -0700

    started hard

---
+++ hard/find_kth_smallest_pair_distance.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "./../imports.hpp"
+
+class Solution
+{
+public:
+    int smallestDistancePair(vector<int>& nums, int k)
+    {
+        sort(nums.begin(), nums.end());
+
+        // for (int i = 0; i < k; )
+    }
+};
