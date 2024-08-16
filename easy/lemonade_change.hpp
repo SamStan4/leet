b58caab8ddf6b36827a6f83156af683760415294
samStan4 <sam.charles.stanley@gmail.com>
2024-08-15 17:25:17 -0700

    start

---
+++ easy/lemonade_change.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "./../imports.hpp"
+
+class Solution
+{
+public:
+    bool lemonadeChange(vector<int>& bills)
+    {
+        int fives = 0, tens = 0;
+
+        for (int i = 0; i < bills.size(); ++i)
+        {
+            
+        }    
+    }
+};
